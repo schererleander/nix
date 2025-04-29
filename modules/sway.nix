2d380a1507c71f7ce303ffd7817b00c82d21ac38
schererleander <leander@schererleander.de>
2025-04-29 22:48:52 +0200

    change keybindings, volume, ...

--- modules/sway.nix
+++ modules/sway.nix
@@ -43,50 +43,8 @@ in {
           titlebar = false;
           border = 0;
         };
-        keybindings = let
-          mod = "Mod4";
-        in
-          lib.mkOptionDefault {
-            "${mod}+Return" = "exec ${pkgs.foot}/bin/foot";
-            "${mod}+q" = "kill";
-            "${mod}+d" = "exec exec ${pkgs.wmenu}/bin/wmenu-run -b -N 000000";
-            "${mod}+Shift+e" = "exec swaynag -t warning -m 'You pressed the exit shortcut. Do you really want to exit sway? This will end your Wayland session.' -B 'Yes, exit sway' 'swaymsg exit'";
-            "${mod}+Tab" = "workspace back_and_forth";
-            "${mod}+1" = "workspace number 1";
-            "${mod}+2" = "workspace number 2";
-            "${mod}+3" = "workspace number 3";
-            "${mod}+4" = "workspace number 4";
-            "${mod}+5" = "workspace number 5";
-            "${mod}+6" = "workspace number 6";
-            "${mod}+7" = "workspace number 7";
-            "${mod}+8" = "workspace number 8";
-            "${mod}+9" = "workspace number 9";
-            "${mod}+0" = "workspace number 10";
-
-            "${mod}+Shift+1" = "move container to workspace number 1";
-            "${mod}+Shift+2" = "move container to workspace number 2";
-            "${mod}+Shift+3" = "move container to workspace number 3";
-            "${mod}+Shift+4" = "move container to workspace number 4";
-            "${mod}+Shift+5" = "move container to workspace number 5";
-            "${mod}+Shift+6" = "move container to workspace number 6";
-            "${mod}+Shift+7" = "move container to workspace number 7";
-            "${mod}+Shift+8" = "move container to workspace number 8";
-            "${mod}+Shift+9" = "move container to workspace number 9";
-            "${mod}+Shift+0" = "move container to workspace number 10";
-
-            "${mod}+b" = "splith";
-            "${mod}+v" = "splitv";
-            "${mod}+f" = "fullscreen";
-            "${mod}+Shift+space" = "floating toggle";
-
-            "XF86AudioPlay" = "exec playerctl play-pause";
-            "XF86AudioPrev" = "exec playerctl previous";
-            "XF86AudioNext" = "exec playerctl next";
-
-            "XF86AudioRaiseVolume" = "exec wpctl set-volume @DEFAULT_AUDIO_DEVICE@ +5%";
-            "XF86AudioLowerVolume" = "exec wpctl set-volume @DEFAULT_AUDIO_DEVICE@ -5%";
-            "XF86AudioMute" = "exec wpctl set-mute @DEFAULT_AUDIO_DEVICE@ toggle";
-          };
+        modifier = "Mod4";
+	terminal = "${pkgs.wmenu}/bin/wmenu-run -b -N 000000";
         defaultWorkspace = "workspace number 1";
         bars = [{
           "command" = "${pkgs.waybar}/bin/waybar";
--- modules/waybar.nix
+++ modules/waybar.nix
@@ -26,22 +26,11 @@ in {
             tooltip-format = "{:%Y-%m-%d | %H:%M}";
           };
           pulseaudio = {
-            format = "{volume}% {icon} {format_source}";
-            format-bluetooth = "{volume}% {icon} {format_source}";
-            format-bluetooth-muted = " {icon} {format_source}";
+            format = "{volume}% {icon}";
+            format-bluetooth = "{volume}% {icon}";
             format-icons = {
-              car = "";
               default = [ "" "" "" ];
-              handsfree = "";
-              headphones = "";
-              headset = "";
-              phone = "";
-              portable = "";
             };
-            format-muted = " {format_source}";
-            format-source = "{volume}% ";
-            format-source-muted = "";
-            on-click = "pavucontrol";
           };
         };
       };
