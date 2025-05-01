b43347117240f9a056003a4bdd2c43bc5c82d189
schererleander <leander@schererleander.de>
2025-05-01 18:55:38 +0200

    add media keybindings

--- modules/sway.nix
+++ modules/sway.nix
@@ -44,7 +44,16 @@ in {
           border = 0;
         };
         modifier = "Mod4";
-	terminal = "${pkgs.wmenu}/bin/wmenu-run -b -N 000000";
+	keybindings = lib.mkOptionDefault {
+	  "XF86AudioPlay" = "exec playerctl play-pause";
+          "XF86AudioPrev" = "exec playerctl previous";
+          "XF86AudioNext" = "exec playerctl next";
+          "XF86AudioRaiseVolume" = "exec wpctl set-volume @DEFAULT_AUDIO_DEVICE@ +5%";
+          "XF86AudioLowerVolume" = "exec wpctl set-volume @DEFAULT_AUDIO_DEVICE@ -5%";
+          "XF86AudioMute" = "exec wpctl set-mute @DEFAULT_AUDIO_DEVICE@ toggle";
+	};
+	menu = "${pkgs.wmenu}/bin/wmenu-run -b -N 000000";
+	terminal = "${pkgs.foot}/bin/foot";
         defaultWorkspace = "workspace number 1";
         bars = [{
           "command" = "${pkgs.waybar}/bin/waybar";
