cea18d4aa42fdb5dea03a61fdaa9dd2f1df11087
schererleander <leander@schererleander.de>
2025-05-01 21:37:34 +0200

    change close behaviour

--- modules/wezterm.nix
+++ modules/wezterm.nix
@@ -5,6 +5,10 @@ let
 in {
   options.wezterm.enable = lib.mkEnableOption "Enable wezterm and setup";
   config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      (nerdfonts.override { fonts = [ "SpaceMono" "IBMPlexMono" "Terminus" ]; })
+    ];
+
     programs.wezterm = {
       enable = true;
       enableZshIntegration = true;
@@ -13,10 +17,12 @@ in {
 
       local config = wezterm.config_builder()
 
+      config.font = wezterm.font "SpaceMono Nerd Font Mono"
+      config.font_size = 10
       config.use_fancy_tab_bar = true
       config.hide_tab_bar_if_only_one_tab = true
-      config.window_background_opacity = 0.8
-      config.exit_behavior = 'Hold'
+      config.window_background_opacity = 0.9
+      config.exit_behavior = 'Close'
       config.exit_behavior_messaging = 'None'
 
       return config
