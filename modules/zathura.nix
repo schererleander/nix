42d64b7990e8a89a557da527e0f1937598636526
schererleander <leander@schererleander.de>
2025-05-02 11:01:42 +0200

    enable recolor

--- modules/zathura.nix
+++ modules/zathura.nix
@@ -8,13 +8,18 @@ in {
     programs.zathura = {
       enable = true;
       options = {
+        recolor-lightcolor = "rgba(0, 0, 0, 0)";
+	recolor-darkcolor = "rgba(255, 255, 255, 1)";
+	recolor = true;
         adjust-open = "width";
+	guioptions = "none";
         zoom-center = true;
         page-padding = 0;
         pages-per-row = 1;
         scroll-page-aware = true;
       };
       mappings = {
+        i = "recolor";
         j = "navigate previous";
         k = "navigate next";
       };
