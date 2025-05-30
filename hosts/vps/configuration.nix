82ce430ecd75ce899beb4c700e3e0c78c8407637
schererleander <leander@schererleander.de>
2025-05-30 06:29:10 +0200

    hardened nginx

--- hosts/vps/configuration.nix
+++ hosts/vps/configuration.nix
@@ -56,7 +56,16 @@
           https   "max-age=31536000; includeSubdomains; preload";
       }
       add_header Strict-Transport-Security $hsts_header;
-      #add_header Content-Security-Policy "script-src 'self'; object-src 'none'; base-uri 'none';" always;
+      add_header Content-Security-Policy
+      "default-src 'self'; \
+       script-src 'self'; \
+       style-src 'self'; \
+       img-src 'self' data:; \
+       font-src 'self'; \
+       connect-src 'self'; \
+       object-src 'none'; \
+       frame-ancestors 'none'; \
+       base-uri 'self';";
       add_header 'Referrer-Policy' 'same-origin';
       add_header X-Frame-Options DENY;
       add_header X-Content-Type-Options nosniff;
