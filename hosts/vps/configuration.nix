39e704e4c5896ac1986ebfdafc7556f80d40b7cb
schererleander <leander@schererleander.de>
2025-05-30 03:01:24 +0200

    remove services

--- hosts/vps/configuration.nix
+++ hosts/vps/configuration.nix
@@ -20,35 +20,6 @@
     enable = true;
   };
 
-  services.nginx = {
-    enable = true;
-    virtualHosts = {
-      root = "/var/www/site/";
-    };
-  };
-
-  services.nextcloud = {
-    enable = true;
-    https = true;
-    hostName = "cloud.schererleander.de";
-    database.createLocally = true;
-    config = {
-      dbtype = "mysql";
-      adminuser = "admin";
-      adminpassFile = "/etc/admin-pass-file";
-    };
-
-    settings = {
-      maintenace_window_start = 2;
-      default_phone_region = "de";
-      filelocking.enabled = true;
-    };
-
-    caching = {
-      redis = true;
-    };
-  };
-
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nixpkgs.config.allowUnfree = true;
   system.stateVersion = "25.05";
