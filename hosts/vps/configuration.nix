00256c5ca303701dcb797019b46e6f2083c5d1c2
schererleander <leander@schererleander.de>
2025-05-30 06:25:28 +0200

    hardened nginx

--- hosts/vps/configuration.nix
+++ hosts/vps/configuration.nix
@@ -57,7 +57,7 @@
       }
       add_header Strict-Transport-Security $hsts_header;
       add_header Content-Security-Policy "script-src 'self'; object-src 'none'; base-uri 'none';" always;
-      add_header 'Referrer-Policy' same-origin always;
+      add_header 'Referrer-Policy' 'same-origin' always;
       add_header X-Frame-Options DENY;
       add_header X-Content-Type-Options nosniff always;
     '';
