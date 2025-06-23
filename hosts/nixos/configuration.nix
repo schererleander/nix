38f45aae3fc574d97784bc9f178e8c385156318f
schererleander <leander@schererleander.de>
2025-06-24 01:20:42 +0200

    reinstall nixos on desktop

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -1,70 +1,90 @@
 { config, pkgs, ... }:
 
 {
-  imports = [
-    ./hardware-configuration.nix
-    ./audio.nix
-    ./wooting.nix
-  ];
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
 
-  # Bootloader
+  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # Use latest kernel
+  # Use latest kernel.
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  # Graphics
-  hardware.graphics = {
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Disable wait online
+  systemd.services.NetworkManager-wait-online.enable = false;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Berlin";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "de_DE.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "de_DE.UTF-8";
+    LC_IDENTIFICATION = "de_DE.UTF-8";
+    LC_MEASUREMENT = "de_DE.UTF-8";
+    LC_MONETARY = "de_DE.UTF-8";
+    LC_NAME = "de_DE.UTF-8";
+    LC_NUMERIC = "de_DE.UTF-8";
+    LC_PAPER = "de_DE.UTF-8";
+    LC_TELEPHONE = "de_DE.UTF-8";
+    LC_TIME = "de_DE.UTF-8";
+  };
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
+  # Configure console keymap
+  console.keyMap = "de";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
     enable = true;
-    enable32Bit = true;
-    extraPackages = with pkgs; [
-      amdvlk
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  users.users.schererleander = {
+    isNormalUser = true;
+    description = "schererleander";
+    extraGroups = [ "networkmanager" "wheel" "video" "input" ];
     ];
   };
 
-   environment.variables.AMD_VULKAN_ICD = "RADV";
+  programs.firefox.enable = true;
+  programs.steam.enable = true;
 
-  # Network
-  networking = {
-    hostName = "nixos";
-    networkmanager.enable = true;
-  };
-
-  # Improve startup time
-  systemd.services.NetworkManager-wait-online.enable = false;
-
-  # Time
-  time.timeZone = "Europe/Berlin";
-
-  # Keymap
-  console.keyMap = "de";
-
-  # User
-  users.users.leander = {
-    isNormalUser = true;
-    hashedPassword = "$6$N1g.gaLo3WGYJA1v$qsd5UCptO/QrtGnMsSaLNVAv2cwKscuwluwl5YslnbdH6gLZ27C10G72py.mO79anKbvL/B0c.RaA6gXyYCk6/";
-    extraGroups = [ "networkmanager" "wheel" "video" "input" ];
-  };
-
-  services = {
-    openssh.enable = true;
-    gnome.gnome-keyring.enable = true;
-  };
-
-  # Security
-  security.polkit.enable = true;
-
-  # home manager crash when disabled
-  programs.dconf.enable = true;
-
-  audio.enable = true;
-  wooting.enable = true;
-
-  users.users.leander.shell = pkgs.zsh;
-  users.users.leander.ignoreShellProgramCheck = true;
-
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = with pkgs; [
+    neovim
+    git
+  ];
+
+  services.openssh.enable = true;
+
+  programs.gnupg.agent = {
+     enable = true;
+     enableSSHSupport = true;
+  };
+
   system.stateVersion = "25.05";
+
 }
