db7e8fb82766f751d809a80b9f8846750d2a1867
schererleander <leander@schererleander.de>
2025-08-02 12:31:18 +0200

    feat: disable vesktop, change plugins

--- modules/home-manager/nixcord.nix
+++ modules/home-manager/nixcord.nix
@@ -8,22 +8,23 @@
   options.nixcord.enable = lib.mkEnableOption "Enable nixcord and setup";
   config = lib.mkIf config.nixcord.enable {
     programs.nixcord = {
-      enable = true; # enable Nixcord. Also installs discord package
+      enable = true;
+      #discord.enable = false;
+      #vesktop.enable = true;
       #quickCss = "some CSS";  # quickCSS file
       config = {
         #useQuickCss = true;   # use our quickCSS
-        #themeLinks = [        # or use an online theme
-        #  "https://raw.githubusercontent.com/link/to/some/theme.css"
-        #];
+        themeLinks = [
+          # or use an online theme
+          "https://refact0r.github.io/system24/theme/system24.theme.css"
+        ];
         frameless = true; # set some Vencord options
         plugins = {
-          hideAttachments.enable = true; # Enable a Vencord plugin
-          ignoreActivities = {
-            # Enable a plugin and set some options
-            enable = true;
-            ignorePlaying = true;
-            ignoreWatching = true;
-          };
+          alwaysAnimate.enable = false;
+          hideAttachments.enable = true;
+          imageLink.enable = true;
+          imageZoom.enable = true;
+          translate.enable = true;
         };
       };
     };
