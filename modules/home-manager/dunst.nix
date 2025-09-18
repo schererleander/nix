b0c76359607a59aa38424dde3979ad1144be1125
schererleander <leander@schererleander.de>
2025-09-18 23:22:31 +0200

    feat: change offset, colors

--- modules/home-manager/dunst.nix
+++ modules/home-manager/dunst.nix
@@ -1,20 +1,41 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 
 {
   options.dunst.enable = lib.mkEnableOption "Enable dunst notification";
   config = lib.mkIf config.dunst.enable {
+    home.packages = with pkgs; [
+      libnotify
+    ];
+
     services.dunst = {
       enable = true;
       settings = {
         global = {
-          transparency = 10;
           font = "monospace 10";
+          offset = "(15, 15)";
+          frame_width = 0;
+        };
+        urgency_low = {
+          foreground = "#FFFFFF";
+          background = "#000000E6";
+        };
+
+        urgency_normal = {
+          foreground = "#FFFFFF";
+          background = "#000000E6";
+        };
+
+        urgency_critical = {
+          foreground = "#FFFFFF";
+          background = "#000000E6";
         };
       };
+
     };
   };
 }
