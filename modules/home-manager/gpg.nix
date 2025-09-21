d112ae7722c1a836cdcfc91b118b2483e808a9f3
schererleander <leander@schererleander.de>
2025-09-22 00:26:13 +0200

    feat: use pinentry_mac on Darwin, pinentry-curses elsewhere

--- modules/home-manager/gpg.nix
+++ modules/home-manager/gpg.nix
@@ -2,16 +2,20 @@
   config,
   pkgs,
   lib,
+  system,
   ...
 }:
 
+let
+  pinentryPackage = if pkgs.stdenv.isDarwin then pkgs.pinentry_mac else pkgs.pinentry-curses;
+in
 {
   options.gpg.enable = lib.mkEnableOption "Setup gpg and agent";
   config = lib.mkIf config.gpg.enable {
     programs.gpg.enable = true;
     services.gpg-agent = {
       enable = true;
-      pinentry.package = pkgs.pinentry-curses;
+      pinentry.package = pinentryPackage;
     };
   };
 }
