27da42dd36aaad7e17a73dc18c92d91d995c9ee8
schererleander <leander@schererleander.de>
2025-10-04 03:52:59 +0200

    fix: remove hideAttachments plugin

--- modules/home-manager/nixcord.nix
+++ modules/home-manager/nixcord.nix
@@ -9,19 +9,13 @@
   config = lib.mkIf config.nixcord.enable {
     programs.nixcord = {
       enable = true;
-      #discord.enable = false;
-      #vesktop.enable = true;
-      #quickCss = "some CSS";  # quickCSS file
       config = {
-        #useQuickCss = true;   # use our quickCSS
         themeLinks = [
-          # or use an online theme
           "https://refact0r.github.io/system24/theme/system24.theme.css"
         ];
-        frameless = true; # set some Vencord options
+        frameless = true;
         plugins = {
           alwaysAnimate.enable = false;
-          hideAttachments.enable = true;
           imageLink.enable = true;
           imageZoom.enable = true;
           translate.enable = true;
