c5d5016b7c39ea385264e4c251b5b954fee7aa56
schererleander <leander@schererleander.de>
2025-11-02 20:42:44 +0100

    enable gnome extensions

--- modules/home-manager/gnome-dconf.nix
+++ modules/home-manager/gnome-dconf.nix
@@ -8,14 +8,16 @@
 {
   options.gnome-dconf.enable = lib.mkEnableOption "Enable gnome dconf settings";
   config = lib.mkIf config.gnome-dconf.enable {
-    packages = with pkgs; [
-      gnomeExtensions.dash-to-panel
-			gnomeExtensions.user-theme
+    home.packages = with pkgs; [
     ];
     dconf = {
       settings = {
-        "/org/gnome/desktop/interface/" = {
-          color-scheme = "'prefer-dark'";
+        "org/gnome/shell" = {
+          enabled-extensions = [
+          ];
+        };
+        "org/gnome/desktop/interface" = {
+          color-scheme = "prefer-dark";
         };
       };
     };
