3d8f74840e74e142d2ca082f10e6a533bae6545e
schererleander <leander@schererleander.de>
2025-11-03 22:27:46 +0100

    marked as broken

--- modules/programs/anki.nix
+++ modules/programs/anki.nix
@@ -1,15 +1,16 @@
 {
   config,
-	username,
+  pkgs,
   lib,
+  username,
   ...
 }:
 
 {
   options.nx.programs.anki.enable = lib.mkEnableOption "Enable anki";
-  config = lib.mkIf config.anki.enable {
-	  #Wait for stable release
-		#home-manager.users.${username}.programs.anki = {
+  config = lib.mkIf config.nx.programs.anki.enable {
+    # Marked as broken
+    #home-manager.users.${username}.programs.anki = {
       #enable = true;
       #style = "native";
       #addons = with pkgs.ankiAddons; [
