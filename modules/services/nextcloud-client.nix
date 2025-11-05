bcf8b6d01c29df57be62435103985e63727dbb0a
schererleander <leander@schererleander.de>
2025-11-05 18:23:13 +0100

    rename option

--- modules/services/nextcloud-client.nix
+++ modules/services/nextcloud-client.nix
@@ -6,8 +6,9 @@
 }:
 
 {
-  options.nextcloud-client.enable = lib.mkEnableOption "Enable and setup nextcloud-client";
-  config = lib.mkIf config.nextcloud-client.enable {
+  options.nx.services.nextcloud-client.enable =
+    lib.mkEnableOption "Enable and setup nextcloud-client";
+  config = lib.mkIf config.nx.services.nextcloud-client.enable {
     home-manager.users."${username}".services.nextcloud-client = {
       enable = true;
       startInBackground = true;
