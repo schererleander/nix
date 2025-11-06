9147307eadcfb9a18afff66b6d824a37bd48d190
schererleander <leander@schererleander.de>
2025-11-06 11:54:40 +0100

    formatting and disable auto updates

--- modules/programs/opencode.nix
+++ modules/programs/opencode.nix
@@ -8,14 +8,12 @@
 {
   options.nx.programs.opencode.enable = lib.mkEnableOption "Setup opencode";
   config = lib.mkIf config.nx.programs.opencode.enable {
-    home-manager.users.${username} = {
-      programs.opencode = {
-        enable = true;
-        settings = {
-          theme = "system";
-          share = "disabled";
-          autoUpdate = true;
-        };
+    home-manager.users.${username}.programs.opencode = {
+      enable = true;
+      settings = {
+        theme = "system";
+        share = "disabled";
+        autoUpdate = false;
       };
     };
   };
