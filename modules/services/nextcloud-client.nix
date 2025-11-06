a6093f79113ff7b3729bc81af4269a2b4037f7ed
schererleander <leander@schererleander.de>
2025-11-06 19:40:15 +0100

    setup nextcloud-client service

--- modules/services/nextcloud-client.nix
+++ modules/services/nextcloud-client.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   username,
   ...
 }:
@@ -9,9 +10,12 @@
   options.nx.services.nextcloud-client.enable =
     lib.mkEnableOption "Enable and setup nextcloud-client";
   config = lib.mkIf config.nx.services.nextcloud-client.enable {
-    home-manager.users."${username}".services.nextcloud-client = {
-      enable = true;
-      startInBackground = true;
+    home-manager.users."${username}" = {
+      home.packages = with pkgs; [ nextcloud-client ];
+      services.nextcloud-client = {
+        enable = true;
+        startInBackground = true;
+      };
     };
   };
 }
