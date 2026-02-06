0070d30074cb71ca745644384d230abeaa0a2d04
Leander Scherer <leander@schererleander.de>
2026-02-06 21:07:03 +0100

    fix(adam): setup gpu drivers

--- modules/hosts/adam/configuration.nix
+++ modules/hosts/adam/configuration.nix
@@ -8,6 +8,7 @@
       boot = {
         kernelPackages = pkgs.linuxPackages_latest;
         kernelParams = [ "amd_pstate=active" ];
+        kernelModules = [ "amdgpu" ];
         initrd.luks.devices."luks-803851e9-7fa8-4367-a927-0bb76d0fe830".device =
           "/dev/disk/by-uuid/803851e9-7fa8-4367-a927-0bb76d0fe830";
         loader = {
@@ -35,6 +36,12 @@
       # Disable waiting for network to be online
       systemd.services.NetworkManager-wait-online.enable = false;
 
+      # Graphics configuration
+      hardware.graphics = {
+        enable = true;
+        enable32Bit = true;
+      };
+
       # User configuration
       users.users.schererleander = {
         isNormalUser = true;
@@ -49,7 +56,12 @@
         ];
       };
 
-      nix.settings.experimental-features = [ "nix-command" "flakes" ];
+      nix.settings.experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+
+      hardware.enableRedistributableFirmware = true;
 
       nixpkgs.config.allowUnfree = true;
       nixpkgs.hostPlatform = "x86_64-linux";
