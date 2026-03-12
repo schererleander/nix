4e63d85bd134952e5004c34647000c1d1f07ca4e
schererleander <leander@schererleander.de>
2026-03-13 00:17:48 +0100

    feat(nextcloud): bump nextcloud version to 33

--- modules/services/nextcloud.nix
+++ modules/services/nextcloud.nix
@@ -9,7 +9,7 @@
     {
       services.nextcloud = {
         enable = true;
-        package = pkgs.nextcloud32;
+        package = pkgs.nextcloud33;
         hostName = "cloud.schererleander.de";
         https = true;
         database.createLocally = true;
