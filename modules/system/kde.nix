321c265d7727918568b5d1067d6076405357ffcb
Leander Scherer <leander@schererleander.de>
2026-03-13 11:10:39 +0100

    feat(kde): customize cursor theme and add utilities

--- modules/system/kde.nix
+++ modules/system/kde.nix
@@ -7,15 +7,24 @@
         enable = true;
         wayland.enable = true;
       };
+
       services.desktopManager.plasma6.enable = true;
       security.pam.services.sddm.enableKwallet = true;
       environment.plasma6.excludePackages = with pkgs.kdePackages; [
         elisa
         kate
       ];
+
+      environment.variables = {
+        XCURSOR_THEME = "Breeze_Snow";
+        XCURSOR_SIZE = "24";
+      };
+
       environment.systemPackages = with pkgs.kdePackages; [
         kcalc
         elisa
+        kcolorchooser
+        kolourpaint
       ];
     };
 }
