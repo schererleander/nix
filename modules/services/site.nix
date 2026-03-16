5cdc699139f3d9687134a6ddbae13f4ab7d055a3
schererleander <leander@schererleander.de>
2026-03-16 17:28:37 +0100

    feat(site): replace personal site with github redirect

--- modules/services/site.nix
+++ modules/services/site.nix
@@ -6,15 +6,24 @@
       ...
     }:
     {
-      imports = [
-        inputs.site.nixosModules.default
-      ];
+      /*
+			imports = [
+				inputs.site.nixosModules.default
+			];
 
-      services.site = {
-        enable = true;
-        domain = "schererleander.de";
+			services.site = {
+				enable = true;
+				domain = "schererleander.de";
+				sslCertificate = config.sops.secrets."cert_fullchain".path;
+				sslCertificateKey = config.sops.secrets."cert_private".path;
+			};
+      */
+
+      services.nginx.virtualHosts."schererleander.de" = {
+        forceSSL = true;
         sslCertificate = config.sops.secrets."cert_fullchain".path;
         sslCertificateKey = config.sops.secrets."cert_private".path;
+        locations."/".return = "301 https://github.com/schererleander";
       };
     };
 }
