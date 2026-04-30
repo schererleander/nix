429fd2afd324c8b2c7e881a98375d2b5bc064580
schererleander <leander@schererleander.de>
2026-04-30 12:12:03 +0200

    chore(zed): cleanup configuration

--- modules/programs/zed.nix
+++ modules/programs/zed.nix
@@ -19,24 +19,10 @@
         };
         title_bar = {
           show_onboarding_banner = false;
-          show_project_items = false;
-          #show_branch_name = false;
+          show_sign_in = false;
           show_user_menu = false;
         };
-        #tab_bar.show = false;
         toolbar.quick_actions = false;
-        #status_bar."experimental.show" = false;
-        project_panel = {
-          dock = "right";
-          default_width = 400;
-          #hide_root = true;
-          auto_fold_dirs = false;
-          #starts_open = false;
-          #git_status = false;
-          sticky_scroll = false;
-          scrollbar.show = "never";
-          indent_guides.show = "never";
-        };
         outline_panel = {
           default_width = 300;
           indent_guides.show = "never";
