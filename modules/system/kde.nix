1cf2bf71f91da231bac7cbd66f2c85220a413c5f
Leander Scherer <leander@schererleander.de>
2026-06-14 23:56:38 +0200

    feat(kde): add scanner, rar support for ark

--- modules/system/kde.nix
+++ modules/system/kde.nix
@@ -3,15 +3,19 @@
     { pkgs, ... }:
     {
       networking.networkmanager.enable = true;
+
       services.displayManager.sddm = {
         enable = true;
         wayland.enable = true;
       };
 
       services.desktopManager.plasma6.enable = true;
+
       security.pam.services.sddm.enableKwallet = true;
+
       environment.plasma6.excludePackages = with pkgs.kdePackages; [
         kate
+        discover
       ];
 
       environment.variables = {
@@ -19,19 +23,25 @@
         XCURSOR_SIZE = "24";
       };
 
-      environment.systemPackages = with pkgs.kdePackages; [
-        (spectacle.override {
-          tesseractLanguages = [
-            "eng"
-            "deu"
-          ];
-        })
-        partitionmanager
-        filelight
-        isoimagewriter
-        kcalc
-        kcolorchooser
-        kolourpaint
-      ];
+      environment.systemPackages =
+        (with pkgs.kdePackages; [
+          (spectacle.override {
+            tesseractLanguages = [
+              "eng"
+              "deu"
+            ];
+          })
+          partitionmanager
+          filelight
+          isoimagewriter
+          kcalc
+          kcolorchooser
+          kolourpaint
+          skanpage
+        ])
+        ++ (with pkgs; [
+          unrar
+          rar
+        ]);
     };
 }
