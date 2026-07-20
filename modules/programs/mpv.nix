6d968acd50e63e70cd1d224149023590109e619f
Leander Scherer <leander@schererleander.de>
2026-07-20 22:36:07 +0200

    refactor(mpv): update hdr, dovi and tone mapping settings

--- modules/programs/mpv.nix
+++ modules/programs/mpv.nix
@@ -2,46 +2,51 @@
   flake.modules.homeManager.mpv = {
     programs.mpv = {
       enable = true;
+
       config = {
         vo = "gpu-next";
         gpu-api = "vulkan";
+        gpu-context = "waylandvk";
+
+        target-colorspace-hint = "auto";
+        target-colorspace-hint-mode = "target";
+
+        target-trc = "pq";
+        target-prim = "bt.2020";
         target-peak = 500;
-        target-colorspace-hint = "yes";
+        target-contrast = "inf";
+        hdr-reference-white = 203;
+
+        tone-mapping = "auto";
+        hdr-compute-peak = "auto";
+        inverse-tone-mapping = "no";
       };
+
       bindings = {
         UP = "add chapter 1";
         DOWN = "add chapter -1";
+
+        "CTRL+3" = "apply-profile SDR_HDR_EFFECT";
+        "CTRL+SHIFT+3" = "apply-profile SDR_HDR_EFFECT restore";
       };
+
       profiles = {
-        # Dolby Vision profile
-        "DOVI" = {
+        DOVI = {
           profile-restore = "copy";
-          profile-cond = "p[\"video-dec-params/gamma\"] == \"auto\"";
-          target-trc = "pq";
-          target-prim = "bt.2020";
+          profile-cond = ''
+            get("video-dec-params/colormatrix", "") == "dolbyvision"
+          '';
+
+          tone-mapping = "auto";
+          inverse-tone-mapping = "no";
+        };
+
+        SDR_HDR_EFFECT = {
+          profile-restore = "copy";
+
           target-peak = 500;
-          tone-mapping-mode = "auto";
-        };
-
-        # SDR look while in HDR
-        "SDR" = {
-          profile-restore = "copy";
-          target-trc = "pq";
-          target-prim = "bt.2020";
-          target-peak = 207;
-          tone-mapping = "bt.2390";
-          tone-mapping-mode = "rgb";
-          inverse-tone-mapping = "yes";
-        };
-
-        # SDR to HDR inverse tone mapping
-        "SDR_HDR_EFFECT" = {
-          profile-restore = "copy";
-          target-trc = "pq";
-          target-prim = "bt.2020";
-          target-peak = 406;
+          hdr-reference-white = 203;
           tone-mapping = "spline";
-          tone-mapping-mode = "rgb";
           inverse-tone-mapping = "yes";
         };
       };
