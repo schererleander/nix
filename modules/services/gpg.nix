4b78f2ca8bc882184cb607a930bfc6c71d577565
Leander Scherer <leander@schererleander.de>
2026-09-04 19:38:52 +0200

    fix(gpg): replace deprecated stdenv platform attribute

--- modules/services/gpg.nix
+++ modules/services/gpg.nix
@@ -11,7 +11,8 @@
 
       services.gpg-agent = {
         enable = true;
-        pinentry.package = if pkgs.stdenv.isDarwin then pkgs.pinentry_mac else pkgs.pinentry-qt;
+        pinentry.package =
+          if pkgs.stdenv.hostPlatform.isDarwin then pkgs.pinentry_mac else pkgs.pinentry-qt;
       };
     };
 }
