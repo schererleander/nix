19fdf6693230035b05e9dd492cb9e23916001c7c
Leander Scherer <leander@schererleander.de>
2026-09-20 13:56:49 +0200

    chore(nextcloud): disable mail transport

--- modules/services/nextcloud.nix
+++ modules/services/nextcloud.nix
@@ -29,10 +29,7 @@
           log_type = "file";
           # NixOS already provides its own integrity check and the nix store is read-only, therefore Nextcloud does not need to do its own integrity checks.
           "integrity.check.disabled" = true;
-          mail_smtpmode = "sendmail";
-          mail_sendmailmode = "pipe";
-          mail_from_address = "noreply";
-          mail_domain = "cloud.schererleander.de";
+          mail_smtpmode = "null";
         };
         phpOptions."opcache.interned_strings_buffer" = "32";
       };
